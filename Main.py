a3b444b2700cf4d64e9e86775d056c871f1bb2b8
q.ntshangase <qntshangase023@student.wethinkcode.co.za>
2024-07-05 09:27:03 +0200

    Added car race image.

--- Main.py
+++ Main.py
@@ -1,10 +1,28 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('A BIT RACEY')
 
+# Game Display
+display_width = 800
+display_height = 600
+
+# Color
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+
+def car(x, y):
+    gameDisplay.blit(carImg, (x, y))
+
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('A BIT RACEY')
 clock = pygame.time.Clock()
+carImg = pygame.image.load('RaceCar.png')
 
 crashed = False
 
