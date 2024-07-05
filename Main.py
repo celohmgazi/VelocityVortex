e977a5eedbc0d7f2810d285aa147abcd0f9efda8
q.ntshangase <qntshangase023@student.wethinkcode.co.za>
2024-07-05 12:40:50 +0200

    GAME LOOP EXIT THE GAME WHEN EDGES ARE TOUCHED

--- Main.py
+++ Main.py
@@ -11,29 +11,58 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
+car_width = 73
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('A BIT RACEY')
+clock = pygame.time.Clock()
+
+try:
+    carImg = pygame.image.load('RaceCar.png')
+    carImg = pygame.transform.scale(carImg, (80, 100))
+except pygame.error:
+    print("Error: Unable to load image")
+    pygame.quit()
+    quit()
+
 
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
+# Car initial position
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
 
-gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('A BIT RACEY')
-clock = pygame.time.Clock()
-carImg = pygame.image.load('RaceCar.png')
+    x_change = 0
 
-crashed = False
+    gameExit = False
 
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        print(event)
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
 
-    pygame.display.update()
-    clock.tick(60)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
 
+        x += x_change
+
+        gameDisplay.fill(white)
+        car(x, y)
+        if x > display_width - car_width or x < 0:
+            gameExit = True
+        pygame.display.update()
+        clock.tick(60)
+
+
+game_loop()
 pygame.quit()
 quit()
